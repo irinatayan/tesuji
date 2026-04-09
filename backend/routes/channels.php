241505cd679723a4f434c019b6669eea67383e9f
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2026-04-09 12:14:21 +0300

    feat(broadcast): add channel authorization for game.{id}

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Models\Game;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('game.{gameId}', function (User $user, int $gameId): bool {
+    $game = Game::find($gameId);
+
+    return $game !== null && (
+        $user->id === $game->black_player_id ||
+        $user->id === $game->white_player_id
+    );
 });
