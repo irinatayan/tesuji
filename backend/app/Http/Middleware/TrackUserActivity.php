10dfb14a061853404b9d050d6ea24e89f0873342
Irina Tayanovskaya <irina.tayanovskaya@gmail.com>
2026-04-20 16:24:27 +0300

    fix: add missing TrackUserActivity middleware

---
+++ backend/app/Http/Middleware/TrackUserActivity.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\Response;
+
+final class TrackUserActivity
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($user = $request->user()) {
+            Cache::put("user_online:{$user->id}", true, now()->addSeconds(30));
+        }
+
+        return $next($request);
+    }
+}
